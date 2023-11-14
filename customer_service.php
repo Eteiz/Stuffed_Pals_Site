4a8b74e16d30f299f499e1a951f9092abecedf93
Marta-Ambroziak <240625@edu.p.lodz.pl>
2023-11-14 20:58:21 +0100

    Adding Customer Service page with FAQ section done

--- customer_service.php
+++ customer_service.php
@@ -24,26 +24,86 @@
 </head>
 <body class="customerservice_page">
 	<?php include 'site_static_parts\navbar.php'; ?>
+    <header>
+        <div class="header-content white_background">
+            <h1> Cutomser Service </h1>
+            <h3> Your satisfaction is our priority—reach out to us with any questions or for support, 
+                and we'll ensure you find all the answers and assistance you need. 
+            </h3>
+        </div>
+    </header>
     <main>
-        <section class="titular_section">
-            <div class="titular_section_container white_background">
-                <h1> Cutomser Service </h1>
-                <h3> Your satisfaction is our priority—reach out to us with any questions or for support, 
-                    and we'll ensure you find all the answers and assistance you need. 
-                </h3>
+        <section id="FAQ-section" class="white_background">
+            <div class="section-header">
+                <h1> Frequently Asked Questions </img></h1>
             </div>
-        </section>
-        <section class="contactus_section blue_pink_background">
-            <div class="contactus_section_container">
-                <img src="img/plush_3.png" alt="Plushie_3 Image"></img>
-                <div class="contactus_section_container_descriptions">
-                    
+            <div class="section-content white_background">
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> What products does Stuffed Pals offer? </h3></div>
+                    <div class="section-content-row-description">
+                        Stuffed Pals offers a unique selection of customizable plushies. Our range includes plush bases, a variety of plushie clothes, and numerous accessories to personalize your plush pal.
+                    </div>
                 </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> How do I customize a plushie? </h3></div>
+                    <div class="section-content-row-description">
+                        Customizing a plushie is simple and fun! First, select a plush base from our collection. 
+                        Next, choose from our range of clothes and accessories to create a look that's uniquely yours. 
+                        You can mix and match items to your heart's content.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> Are the materials used for the plushies safe? </h3></div>
+                    <div class="section-content-row-description">
+                        Absolutely! Safety is our top priority. 
+                        All materials used in our plushies, clothes, and accessories are non-toxic and comply with international safety standards.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> Can I wash the plushies? </h3></div>
+                    <div class="section-content-row-description">
+                        Yes, our plushies are washable. We recommend hand washing with mild detergent and air drying to maintain their shape and color.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> What age group are these plushies suitable for? </h3></div>
+                    <div class="section-content-row-description">
+                        Our plushies are designed for all ages! They are particularly popular among children and collectors.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> How long does shipping take? </h3></div>
+                    <div class="section-content-row-description">
+                        Shipping times vary depending on your location. 
+                        Typically, orders are processed within 1-2 business days, and delivery takes 3-5 business days.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> Do you offer international shipping? </h3></div>
+                    <div class="section-content-row-description">
+                        Yes, we ship worldwide! Shipping fees and times will vary based on the destination.
+                    </div>
+                </div>
+                <hr>
+                <div class="section-content-row">
+                    <div class="section-content-row-header"><h3> What is your return policy? </h3></div>
+                    <div class="section-content-row-description">
+                        We accept returns within 30 days of purchase, provided the items are in their original condition. 
+                        Customized items are subject to a separate return policy, detailed on our website.
+                    </div>
+                </div>
+                <hr>
             </div>
         </section>
-        
 		<?php include 'site_static_parts\newsletter_form.php'; ?>
 	</main>
 	<?php include 'site_static_parts\footer.php'; ?>
+    <script src="js_scripts\faq_accordion.js"></script>
 </body>
 </html>
---
+++ js_scripts/faq_accordion.js
@@ -0,0 +1,8 @@
+var faq = document.querySelectorAll("#FAQ-section .section-content-row-header");
+for (var i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("active-faq");
+        var body = this.nextElementSibling;
+        body.classList.toggle("open");
+    });
+}
